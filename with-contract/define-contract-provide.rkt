8702d81e4f7c42c2f252c98e655b6dd7747d92df
AlexKnauth <alexander@knauth.org>
2015-10-31 23:12:31 -0400

    add define/contract/provide+unsafe

--- with-contract/define-contract-provide.rkt
+++ with-contract/define-contract-provide.rkt
@@ -1,9 +1,11 @@
 #lang racket/base
 
 (provide define/contract/provide
+         define/contract/provide+unsafe
          )
 
 (require racket/contract/base
+         "provide-unsafe.rkt"
          (for-syntax racket/base
                      syntax/parse
                      ))
@@ -16,5 +18,15 @@
          (define id val))]
     [(define/contract/provide (head:expr . args) ctc:expr body:expr ...+)
      #'(define/contract/provide head ctc (lambda args body ...))]
+    ))
+
+(define-syntax define/contract/provide+unsafe
+  (syntax-parser
+    [(define/contract/provide id:id ctc:expr val:expr)
+     #'(begin
+         (provide/contract+unsafe [id ctc])
+         (define id val))]
+    [(define/contract/provide+unsafe (head:expr . args) ctc:expr body:expr ...+)
+     #'(define/contract/provide+unsafe head ctc (lambda args body ...))]
     ))
 
